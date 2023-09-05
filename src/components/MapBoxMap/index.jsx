b1423890bda034d2522fce9acdbe045392b6a913
yauheni-prokharau <eugene.prokhorov12@gmail.com>
2023-09-05 16:25:55 +0300

    feat: add custom elasticsearch

--- src/components/MapBoxMap/index.jsx
+++ src/components/MapBoxMap/index.jsx
@@ -2,11 +2,29 @@ import React, { Component } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+import { quotesData } from "@constants";
+
+import {
+  StyledInputContainer,
+  StyledTitle,
+  StyledInput,
+  StyledList,
+  StyledListItem,
+} from "./styled";
+
 class MapBoxMap extends Component {
   constructor(props) {
     super(props);
     this.mapContainer = React.createRef();
     this.map = null;
+
+    this.state = {
+      searchInput: "",
+      searchResults: [],
+    };
+
+    this.searchQuotes = this.searchQuotes.bind(this);
+    this.handleSearchInputChange = this.handleSearchInputChange.bind(this);
   }
 
   componentDidMount() {
@@ -26,13 +44,53 @@ class MapBoxMap extends Component {
     }
   }
 
+  searchQuotes(searchInput) {
+    const results = quotesData.filter(
+      (quote) =>
+        quote.code.toLowerCase().includes(searchInput.toLowerCase()) ||
+        quote.text.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    this.setState({
+      searchResults: results,
+    });
+  }
+
+  handleSearchInputChange(e) {
+    const { value } = e.target;
+    this.setState({ searchInput: value });
+    this.searchQuotes(value);
+  }
+
   render() {
+    const { searchInput, searchResults } = this.state;
+
     return (
-      <div
-        ref={this.mapContainer}
-        className="map-container"
-        style={{ width: "100%", height: "400px" }}
-      />
+      <section>
+        <StyledTitle>Search currency in the bank</StyledTitle>
+        <StyledInputContainer>
+          <StyledInput
+            type="text"
+            placeholder="Currency search..."
+            value={searchInput}
+            onChange={this.handleSearchInputChange}
+          />
+          {searchInput && (
+            <StyledList>
+              {searchResults.map((result) => (
+                <StyledListItem key={result.id}>
+                  {result.text} ({result.code})
+                </StyledListItem>
+              ))}
+            </StyledList>
+          )}
+        </StyledInputContainer>
+        <div
+          ref={this.mapContainer}
+          className="map-container"
+          style={{ width: "100%", height: "400px" }}
+        />
+      </section>
     );
   }
 }
---
+++ src/components/MapBoxMap/styled.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const StyledInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+export const StyledTitle = styled.h2`
+  text-align: center;
+  font-size: 38px;
+  font-weight: 400;
+  margin-bottom: 20px;
+`;
+
+export const StyledInput = styled.input`
+  background: rgba(27, 32, 40, 1);
+  width: 450px;
+  height: 65.5px;
+  padding: 10px;
+  padding-left: 20px;
+  border: 1px solid rgba(204, 204, 204, 1);
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  font-size: 16px;
+  outline: none;
+  margin-bottom: 40px;
+`;
+
+export const StyledList = styled.ul`
+  position: absolute;
+  z-index: 1;
+  top: calc(80%);
+  left: 50%;
+  transform: translateX(-50%);
+  border: 1px solid rgba(204, 204, 204, 1);
+  border-radius: 4px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  list-style: none;
+  padding: 0;
+  max-height: 200px;
+  overflow-y: auto;
+`;
+
+export const StyledListItem = styled.li`
+  padding: 8px;
+  border-bottom: 1px solid rgba(204, 204, 204, 0.9);
+  background: rgba(27, 32, 40, 1);
+  cursor: pointer;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
