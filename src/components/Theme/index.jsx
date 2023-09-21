dc8257c40e361f8ea3297a8bc112baa43d8af9a9
yauheni-prokharau <eugene.prokhorov12@gmail.com>
2023-09-21 10:59:21 +0300

    fix: attemp to fix theme change issue

--- src/components/Theme/index.jsx
+++ src/components/Theme/index.jsx
@@ -1,23 +1,15 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import PropTypes from "prop-types";
 
 import { themes } from "@theme";
 import { Global } from "@components/App/styled";
-import { toggleTheme } from "@store/reducers/themeSlice";
 
 const Theme = ({ children }) => {
-  const dispatch = useDispatch();
   const theme = useSelector((state) => state.theme);
 
   const selectedTheme = themes[theme];
-
-  useEffect(() => {
-    if (selectedTheme && selectedTheme !== localStorage.getItem("theme")) {
-      dispatch(toggleTheme(selectedTheme));
-    }
-  }, []);
 
   return (
     <ThemeProvider theme={selectedTheme}>
