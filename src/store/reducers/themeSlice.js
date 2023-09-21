35b69c72767a9c5e41bdda57a804b7f8ea6ae0e4
yauheni-prokharau <eugene.prokhorov12@gmail.com>
2023-09-21 09:39:47 +0300

    fix: attemp to fix theme change issue

--- src/store/reducers/themeSlice.js
+++ src/store/reducers/themeSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const savedTheme = localStorage.getItem("theme") || "dark";
+const savedTheme = localStorage.getItem("theme");
 
 const themeSlice = createSlice({
   name: "theme",
